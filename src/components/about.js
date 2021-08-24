85b741a3963ca3c17736231d241c69f1bca13bcd
Kutay Kağan Özen <63947758+Iseluin@users.noreply.github.com>
2021-08-24 08:06:14 +0300

    Add files via upload
    first commit

---
+++ src/components/about.js
@@ -0,0 +1,54 @@
+import React, { Fragment } from 'react'
+
+import styled from 'styled-components'
+import media from 'utils/media-queries'
+
+import { color, fontSize } from 'styles/theme'
+
+import TwoColumns from 'components/twoColumns'
+import SectionHeading from 'components/sectionHeading'
+
+const Big = styled.span`
+  font-size: ${fontSize.f6};
+  color: ${color.grey900};
+  font-weight: 700;
+  letter-spacing: -0.4px;
+  line-height: 1.35;
+  ${media.lg`
+    font-size: ${fontSize.f5};
+    letter-spacing: -0.3px;
+  `}
+  ${media.sm`
+    font-size: ${fontSize.f5};
+  `}
+`
+
+const About = () => {
+  return (
+    <TwoColumns
+      leftColumn={<SectionHeading>About</SectionHeading>}
+      rightColumn={
+        <Fragment>
+          <Big>
+            Hi. I'm Kutay Kağan, a FrontEnd Developer living in&nbsp;İstanbul. I bring
+            ideas to life with code and&nbsp;design.
+          </Big>
+          <p>
+            In my work I strive to optimize the aesthetic value and usability of
+            products without losing sight of the technical challenges and
+            limitations.
+          </p>
+          <p>
+            I am currently developing my capstone project for the online FrontEnd Development bootcamp by <a target = "_blank" href="https://www.re-coded.com/">Recoded</a>.
+          </p>
+          <p style={{ marginBottom: 0 }}>
+            Outside of work I'm passionate about coding in general, guitars,
+            history and occasional hiking.
+          </p>
+        </Fragment>
+      }
+    />
+  )
+}
+
+export default About
---
+++ src/components/footer.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+import media from 'utils/media-queries'
+
+import { fontSize } from 'styles/theme'
+
+import Section from 'components/section'
+
+const FooterText = styled.div`
+  text-align: center;
+  font-size: ${fontSize.f2};
+  ${media.lg`
+    font-size: ${fontSize.f1};
+  `}
+  ${media.sm`
+    text-align: left;
+    font-size: ${fontSize.f1};
+  `}
+`
+
+const Footer = () => {
+  return (
+    <Section>
+      <FooterText>
+        Check me out on&nbsp;
+        <a href="https://github.com/Iseluin">Github</a>
+      </FooterText>
+    </Section>
+  )
+}
+
+export default Footer
